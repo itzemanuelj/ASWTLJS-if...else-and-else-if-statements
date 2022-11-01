0bc778ab3d8046c2dffb1e16f07ce61823fe4834
Emanuel Joness <itzemanuelj@gmail.com>
2022-11-01 14:20:19 -0400

    inital commit

---
+++ script.js
@@ -0,0 +1,85 @@
+// What keyword specifies an action when all tests above have failed?
+if (a = b) {
+    console.log('');
+}
+else ('');
+//else
+
+// What are the keywords that test for a condition when all tests above it have failed?
+if (a = b) {
+
+}
+else if (a = c);
+//else if
+
+// In one word, what type of statement is formatted the same way as else and else if?
+if (a = b);
+//if
+
+// What is the last line of an else block?
+if (condition1) {
+
+}
+else if (condition2) {
+    //logic
+}
+
+// Create an else block that assigns the value of b to a.
+if (b != a) {
+    //logic
+}
+else b === a;
+
+// Code an else block that displays an alert saying "Nope".
+if (a = b) {
+}
+else alert('Nope');
+
+// Code an else if block that tests whether a has the same value as b. If so, display an alert that says "OK".
+if (a = b) {
+    alert('OK');
+}
+else if (a !== b) {
+    //logic
+}
+
+// Code an else block that assigns the value of one variable to another
+if (cost = time) {
+    //logic;
+}
+else { 
+    time = travel 
+}
+
+    //or
+
+if(num1 = num2) {
+    //logic 
+}
+else {
+    num2 = num1;
+  }
+
+// Code an else block that assigns a number to a variable and displays an alert specifying a string as the message. The variable has been declared beforehand.
+if (time = 1) {
+    alert('lorem');
+}
+else{
+    time > 1
+    alert('time');
+}
+
+// Code a block, that, if tests above it fail, checks whether one variable doesn't equal another and, if so, displays a prompt that assigns the user's response to a variable that has been declared beforehand.
+// if if else else
+// 2 vars !== 
+
+// if else alert('')
+
+name = prompt('enter your name');
+
+if (name = 'justin') {
+    alert('hello ' + name);
+}
+else  { name !== name 
+    alert('hello' + name);
+}
